ab3306d1216bc1c855757cab1f0841e367affba0
Tesmi <88522894+Tesmi-Develop@users.noreply.github.com>
2025-10-24 00:05:33 +1000

    fix: player-data-handler imports

--- packages/player-data-handler/src/index.ts
+++ packages/player-data-handler/src/index.ts
@@ -2,3 +2,6 @@ export * from "./profile-wrapper";
 export * from "./decorators/saved";
 export * from "./systems/saving-system";
 export type * from "./types";
+import("./serilizator/transformers/entity-transformer").expect();
+import("./serilizator/transformers/roblox-type-transformers").expect();
+import("./serilizator/transformers/table-transformer").expect();
